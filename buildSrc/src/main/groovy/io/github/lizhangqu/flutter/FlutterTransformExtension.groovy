39ff74ef56ce06d36ea171808d961dd5a8b66f2f
李樟取 <lizhangqu@weidian.com>
2019-03-22 13:51:20 +0800

    * extension

---
+++ buildSrc/src/main/groovy/io/github/lizhangqu/flutter/FlutterTransformExtension.groovy
@@ -0,0 +1,13 @@
+package io.github.lizhangqu.flutter
+
+/**
+ * @author lizhangqu
+ * @version V1.0
+ * @since 2019-03-22 12:52
+ */
+class FlutterTransformExtension {
+    String patchClass = "io.github.lizhangqu.flutter.FlutterUpdate"
+    String downloadUrlMethod = "getDownloadURL"
+    String installModeMethod = "getInstallMode"
+    String downloadModeMethod = "getDownloadMode"
+}
