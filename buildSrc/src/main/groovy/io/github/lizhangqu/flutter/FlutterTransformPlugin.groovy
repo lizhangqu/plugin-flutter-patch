5e20dd2894541f523cbf358653d65547fdf3820e
李樟取 <lizhangqu@weidian.com>
2019-03-22 13:36:03 +0800

    * plugin

---
+++ buildSrc/src/main/groovy/io/github/lizhangqu/flutter/FlutterTransformPlugin.groovy
@@ -0,0 +1,18 @@
+package io.github.lizhangqu.flutter
+
+import com.android.build.gradle.BaseExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * @author lizhangqu
+ * @version V1.0
+ * @since 2019-03-22 12:52
+ */
+class FlutterTransformPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+       
+    }
+}
